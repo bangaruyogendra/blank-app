c606565b3fd3caed061782e59485a4a378425068
bangaruyogendra <111693322+bangaruyogendra@users.noreply.github.com>
2025-05-29 06:25:08 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 st.title("Student Ranking System")
-rank_table = pd.DataFrame((200,10),
+rank_table = pd.DataFrame(
                          columns = ("Name","English","Hindi","Telugu","Maths","Science","Total Marks","Percentage","Rank")
                          )
 st.write(
